63887a37702fe4a56b54e8282335c52e7b8ec160
naim6246 <uddinnaim895@gmail.com>
2021-07-11 19:05:36 +0600

    added tokenGenerate func & authentication func

--- Back End/auth/authentication.go
+++ Back End/auth/authentication.go
@@ -1,14 +1,86 @@
 package auth
 
-import "github.com/go-chi/chi/v5"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"strconv"
+	"strings"
+	"time"
+	"travelBD/models"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 type IAuth interface {
 }
 
-type Auth struct{}
+const (
+	secret          = "TraVelBdSecR3t"
+	KeyUserID       = "id"
+	KeyTokenExpired = "exp"
+)
 
-func NewAuth() IAuth {
-	return &Auth{}
+type Auth struct {
+	Secret string
 }
 
-func (h *Auth) Handle(router chi.Router) {}
+func NewAuth(auth *Auth) IAuth {
+	return &Auth{
+		Secret: secret,
+	}
+}
+
+func (auth *Auth) GenerateToken(user *models.UserDomain) (string, error) {
+	token := jwt.New(jwt.SigningMethodES256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims[KeyUserID] = user.ID
+	claims[KeyTokenExpired] = time.Now().Add(time.Minute * 30).Unix()
+	signedToken, err := token.SignedString([]byte(auth.Secret))
+	if err != nil {
+		return "", err
+	}
+	return signedToken, nil
+}
+
+func (auth *Auth) Authentication(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("content-type", "application/json")
+		extractToken := func() string {
+			bearerToken := r.Header.Get("Authorization")
+			strArr := strings.Split(bearerToken, " ")
+			if len(strArr) == 2 {
+				return strArr[1]
+			}
+			return ""
+		}
+		if r.Header["Authorization"] != nil {
+			tokenString := extractToken()
+			token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+				if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, errors.New("Invalid authorization token")
+
+				}
+				return []byte(auth.Secret), nil
+			})
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				json.NewEncoder(w).Encode(err)
+				return
+			}
+			if token.Valid {
+				claims := token.Claims.(jwt.MapClaims)
+				r.Header.Set(KeyUserID, strconv.FormatFloat((claims[KeyUserID]).(float64), 'f', 0, 64))
+				handler.ServeHTTP(w, r)
+			} else {
+				w.WriteHeader(http.StatusUnauthorized)
+				json.NewEncoder(w).Encode(errors.New("Invalid authorization token"))
+				// return
+			}
+		} else {
+			w.WriteHeader(http.StatusUnauthorized)
+			json.NewEncoder(w).Encode(errors.New("invalid authorization token"))
+			// return/
+		}
+	})
+}
