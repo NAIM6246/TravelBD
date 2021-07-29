507d280cee8a43c926049f32f4565375fff593a4
naim6246 <uddinnaim895@gmail.com>
2021-07-30 02:57:51 +0600

    changed default start page.start page load according to login status

--- app/src/main/java/com/example/travelbd/MainActivity.java
+++ app/src/main/java/com/example/travelbd/MainActivity.java
@@ -10,25 +10,25 @@ import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static  int SPLASH_TIME_OUT = 3000;
+    private static  int SPLASH_TIME_OUT = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        new Handler().postDelayed(new Runnable() {
-//            @Override
-//            public void run() {
-//                SharedPreferences user = getApplicationContext().getSharedPreferences("user", Context.MODE_PRIVATE);
-//                boolean isLoggedIn = user.getBoolean("isLoggedIn",false);
-//                if(isLoggedIn){
-//                    startActivity(new Intent(MainActivity.this,Auth_Home.class));
-//                }
-//                else {
-//                    startActivity(new Intent(MainActivity.this,Home.class));
-//                }
-//                finish();
-//            }
-//        },SPLASH_TIME_OUT);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SharedPreferences user = getApplicationContext().getSharedPreferences("user", Context.MODE_PRIVATE);
+                boolean isLoggedIn = user.getBoolean("isLoggedIn",false);
+                if(isLoggedIn){
+                    startActivity(new Intent(MainActivity.this,Auth_Home.class));
+                }
+                else {
+                    startActivity(new Intent(MainActivity.this,Home.class));
+                }
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
