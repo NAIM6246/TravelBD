d5727a33d84a3f1f1304249a7decce38acda5370
naim6246 <uddinnaim895@gmail.com>
2021-07-30 11:37:37 +0600

    added user profile icon

--- app/src/main/java/com/example/travelbd/Auth_Home.java
+++ app/src/main/java/com/example/travelbd/Auth_Home.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -46,10 +47,28 @@ public class Auth_Home extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void openUserProfile(){
+        Intent intent = new Intent(this,UserProfile.class);
+        startActivity(intent);
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main,menu);
         return super.onCreateOptionsMenu(menu);
+    }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.actio_profile) {
+            item.setOnMenuItemClickListener(new MenuItem.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    openUserProfile();
+                    return true;
+                }
+            });
+        }
+        return(super.onOptionsItemSelected(item));
     }
 }
 
--- app/src/main/java/com/example/travelbd/Bandarban.java
+++ app/src/main/java/com/example/travelbd/Bandarban.java
@@ -3,6 +3,8 @@ package com.example.travelbd;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class Bandarban extends AppCompatActivity {
 
