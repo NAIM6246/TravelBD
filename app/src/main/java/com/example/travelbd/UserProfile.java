02c29e6f196db6e115a58e7b67d72a116a8e6649
naim6246 <uddinnaim895@gmail.com>
2021-07-30 12:33:47 +0600

    added logout option for user

--- app/src/main/java/com/example/travelbd/UserProfile.java
+++ app/src/main/java/com/example/travelbd/UserProfile.java
@@ -2,13 +2,37 @@ package com.example.travelbd;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class UserProfile extends AppCompatActivity {
-
+    Button logoutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        logoutBtn = findViewById(R.id.logout);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences userPref = getSharedPreferences("user", Context.MODE_PRIVATE);
+                SharedPreferences.Editor editor = userPref.edit();
+                editor.clear();
+                editor.apply();
+                openHome();
+
+            }
+        });
+    }
+
+    public void openHome(){
+        Intent intent = new Intent(this,Home.class);
+        startActivity(intent);
+        finish();
     }
 }
